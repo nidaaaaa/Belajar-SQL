490449e8aaa5b8fcceacfc4465ad3cb376b2abef
Nida Fauziah <89292973+nidaaaaa@users.noreply.github.com>
2021-08-23 18:55:54 +0700

    file tambahan

--- Fundamental SQL Using FUNCTION and GROUP BY.sql
+++ Fundamental SQL Using FUNCTION and GROUP BY.sql
@@ -1,6 +1,6 @@
 #Penggunaan fungsi umum skalar matematika
-Use DQLab 
-Table students 
+#Use DQLab 
+#Table students 
 
 #ABS()
 SELECT StudentID, FirstName, LastName, Semester1, Semester2, 
@@ -63,3 +63,54 @@ SELECT StudentID,
 UPPER(FirstName) as FirstName,
 LOWER(LastName) as LastName
 FROM students;
+
+Fungsi aggregate :
+
+#Sum()
+SELECT SUM(Semester1) as Total_1, SUM(Semester2) as Total_2
+FROM students;
+
+#Count()
+SELECT COUNT(FirstName) as Total_Student
+FROM students;
+
+#AVG()
+SELECT AVG(Semester1) as AVG_1, AVG(Semester2) as AVG_2
+FROM students;
+
+#Min() & Max()
+SELECT MIN(Semester1) as MIN1, MAX(Semester1) as MAX1, MIN(Semester2) as MIN2, MAX(Semester2) as MAX2
+FROM students;
+
+#Penggunaan Group By
+#Use DQLab 
+#Table data_penjualan
+
+#Single column
+SELECT province,
+COUNT(DISTINCT order_id) as total_order,
+SUM(item_price) as total_price
+FROM sales_retail_2019
+GROUP BY province;
+
+#Multi Column
+SELECT province, brand,
+COUNT(DISTINCT order_id) as total_order,
+SUM(item_price) as total_price FROM sales_retail_2019
+GROUP BY province, brand;
+
+#Aggregate & Group By 
+SELECT province, COUNT(DISTINCT order_id) AS total_unique_order,
+SUM(item_price) AS revenue FROM sales_retail_2019
+GROUP BY province;
+
+#Penggunaan Case...When 
+#table sales_retail_2019
+SELECT MONTH(order_date) AS order_month, SUM(item_price) AS total_price, 
+CASE  
+    WHEN sum(item_price) >= 30000000000 THEN 'Target Achieved'
+    WHEN sum(item_price) <= 25000000000 THEN 'Less performed'
+    ELSE 'Follow Up'
+END as remark
+FROM sales_retail_2019
+GROUP BY MONTH(order_date);
